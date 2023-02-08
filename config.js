5a48b7656f533d28dfbe026374be7f0147a12f82
joesanchez-tn <108155257+joesanchez-tn@users.noreply.github.com>
2023-02-08 17:07:18 -0600

    Second edit

--- config.js
+++ config.js
@@ -6,9 +6,7 @@ const config = {
     trailingSlash: false,
   },
   header: {
-    title: "Etycs",
-    logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
-    logoLink: 'https://hasura.io/learn/',
+
     title:
       "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
     githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
